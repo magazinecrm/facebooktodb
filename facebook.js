9594295a690244fd12f8fe779b408f773d28e45b
pjohny <34673267+magazinecrm@users.noreply.github.com>
2020-08-21 18:14:38 +0530

    facebook.js
    this code is uspposed to put data in firebase db

---
+++ facebook.js
@@ -0,0 +1,60 @@
+import React, {Component,useEffect}  from 'react';
+import FacebookLogin from 'react-facebook-login';
+import firebaseDb from  "../firebase";
+
+export default class Facebook extends Component {
+
+    state = {
+        auth: false,
+        name: '',
+        picture: ''
+    };
+
+    responseFacebook = response => {
+        console.log(response);
+        if(response.status !== 'unknown')
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url
+        });
+
+    }
+
+    componentClicked = () => {
+        console.log('Facebook btn clicked');
+    }
+
+    render(){
+        let facebookData;
+var usersRef =  firebaseDb.child('users');
+var timestamp = new Date().getTime();
+usersRef.child(`timestamp`).set(timestamp);
+usersRef.child(`nmaex`).set({this.state.name});
+        this.state.auth ?
+        facebookData = (
+                <div style={{
+                    width: '400px',
+                    margin: 'auto',
+                    background: '#f4f4f4',
+                    padding: '20px',
+                    color: '#000'
+                }}>
+                    <img src={this.state.picture} alt={this.state.name} />
+    <h2>Welcome {this.state.name}! Do you know God is great</h2>
+                </div>
+            ) :
+            facebookData = (<FacebookLogin
+                appId="347072892755118"
+                autoLoad={true}
+                fields="name,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />);
+
+        return (
+            <>
+                {facebookData}
+            </>
+        );
+    }
+}
